7266bd2ed17dab06cc18c67749be97605eab97ac
andrey <andreysouzasilva2@gmail.com>
2024-02-28 13:38:02 -0300

    segunda alteração

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+console.log("hello word")
+let chamadaNumerica = [
+    [1 , 2 , 3 , 4],
+    [5 , 6 , 7 , 8],
+    [9 , 10 , 11, 12]
+]
+console.log("Nós escolhemos uma senha de segurança para você, com esta lista númerica " + chamadaNumerica )
+console.log("A sua senha é " + chamadaNumerica[2][3] + chamadaNumerica[0][0] + chamadaNumerica[1][2] + ", Guarde sua senha por segurança")
